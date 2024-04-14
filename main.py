e45dda79ad112589b72616e7e398fd3e4eb0c1b2
Jorge <jorgejapao00@gmail.com>
2024-04-14 19:22:27 -0300

    Diropenbox added to select upscaled file folder

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageFilter
 import easygui
 import os
 
-def upscaling(take, upscale):
+def upscaling(take, upscale, save):
 
     im = Image.open(take)
 
@@ -16,9 +16,10 @@ def upscaling(take, upscale):
 
     fileName = os.path.basename(take)
 
-    pp.save(f'{upscale}_2z_{fileName}')
+    pp.save(f'{save}'f'{upscale}_2z_{fileName}')
 
 
 take = easygui.fileopenbox()
 upscale = float(input('Upscale how many times? '))
+save = easygui.diropenbox()
 upscaling(take, upscale)
