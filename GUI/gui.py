6e3430e2b65074f10d2678b9c09d09645a946350
Jorge <jorgejapao00@gmail.com>
2024-05-07 09:10:35 -0300

    Base of main screen created

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,1 +1,19 @@
 import customtkinter as ct
+
+def initial(root):
+    print('')
+
+def main():
+    root = ct.CTk()
+    root.geometry("650x700")
+    root.title("Waifu2z - Image Quality Improver")
+    root.after(1, lambda :root.iconbitmap(''))
+
+    initial(root)
+    
+    root.iconify()
+    root.update()
+    root.deiconify()
+    root.mainloop()
+
+main()
