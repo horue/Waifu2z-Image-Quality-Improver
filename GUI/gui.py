f9a1b49f6692adaac2afc69455ebb60a59bf8c9a
Jorge <jorgejapao00@gmail.com>
2024-05-09 17:21:59 -0300

    Checkbox for color improvement added

--- GUI/gui.py
+++ GUI/gui.py
@@ -4,6 +4,12 @@ def initial(root):
     l1 = ct.CTkLabel(root, text="Waifu2z - Image Quality Improver")
     l1.pack(pady=20)
 
+
+    check_var = ct.StringVar(value='off')
+    c1 = ct.CTkCheckBox(root, text='Color improvement', checkbox_width=15, checkbox_height=15, corner_radius=0, border_width=2, variable=check_var, onvalue='on', offvalue='off')
+    c1.pack(pady=10)
+
+
 def main():
     root = ct.CTk()
     root.geometry("650x400")
